23d17fa5ac063018e0f1c93aab76fb1f642256fd
Second <second@Brynns-MacBook-Air.local>
2022-04-05 14:26:02 -0500

    md files

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// display current date and day
+var todayDate = moment ().format('dddd, MMM Do YYYY');
+$("currentDay").html(todayDate);
+
+$(document).ready(function(){
+    // saveBTN click listener
+    $(".saveBtn").on("click",function(){
+        
+    })
+})
