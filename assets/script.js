76e5d4298b2a16af28e06b18b0e2dbe117a9bbc6
brynnke <100329799+brynnke@users.noreply.github.com>
2022-05-05 21:50:20 -0500

    updated the JS to have the function of displaying the current date

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 // display current date and day
-var todayDate = moment ().format('dddd, MMM Do YYYY');
-$("currentDay").html(todayDate);
+var todayDate = moment().format('dddd, MMM Do YYYY');
+$("#currentDay").html(todayDate);
 
 $(document).ready(function(){
     // saveBTN click listener
